bb3c2cc3b66e780e20a3a630428dc66412b559d4
rchavez2583 <77551911+rchavez2583@users.noreply.github.com>
2021-01-16 19:02:08 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CS133PROJECTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int[,] arr2d = new int[1, 10]
+
+            {
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+             
+
+
+
+
+            };
+
+            for (int Y = 1; Y < 11; Y++)
+            {
+                for (int X = 1; X < 11; X++)
+                {
+                    Console.Write((X,Y) + " ");
+                }
+
+                Console.WriteLine("");
+            }
+
+
+
+
+
+        }
+
+    }
+}
